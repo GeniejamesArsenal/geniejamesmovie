7119ce68e47e8c7c3e8f56707a3457e4677c54dc
Genie James Arsenal <116369864+GeniejamesArsenal@users.noreply.github.com>
2024-12-19 16:22:30 +0800

    Delete lockinspect.js

--- lockinspect.js
+++
@@ -1,3 +0,0 @@
-
-document.addEventListener('contextmenu', event => event.preventDefault());
-document.addEventListener('keydown', function (event) { if (event.ctrlKey && (event.key === 'u' || event.key === 's' || event.key === 'a' || event.key === 'c' || event.key === 'x')) { event.preventDefault(); } if (event.key === 'F12' || event.key === 'I' && event.shiftKey && event.ctrlKey) { event.preventDefault(); } });
